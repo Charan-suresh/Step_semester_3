27eb90cb0363e7fc6be2b6250299f2b91f7f351d
Charan Suresh <charansuresh456@gmail.com>
2026-08-28 16:01:36 +0530

    Add ExtendingEmployee program for Week 3 F2

---
+++ week3/ExtendingEmployee.java
@@ -0,0 +1,71 @@
+class Employee {
+    private String empId;
+    private String empName;
+    private double salary;
+
+    public Employee(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+}
+
+class ManagerEmployee extends Employee {
+    private double teamBonus;
+
+    public ManagerEmployee(String empId, String empName, double salary, double teamBonus) {
+        super(empId, empName, salary);
+        this.teamBonus = teamBonus;
+    }
+
+    public double effectiveSalary() {
+        return getSalary() + teamBonus;
+    }
+}
+
+class InternEmployee extends Employee {
+    private double stipendCap;
+
+    public InternEmployee(String empId, String empName, double salary, double stipendCap) {
+        super(empId, empName, salary);
+        this.stipendCap = stipendCap;
+    }
+
+    public double effectiveSalary() {
+        return Math.min(getSalary(), stipendCap);
+    }
+}
+
+public class ExtendingEmployee {
+    public static void main(String[] args) {
+        Employee plain = new Employee("E101", "Alice", 40000.0);
+        Employee mgr = new ManagerEmployee("M102", "Bob", 70000.0, 8000.0);
+        Employee intern = new InternEmployee("I103", "Charlie", 12000.0, 10000.0);
+
+        Employee[] employees = new Employee[] { plain, mgr, intern };
+
+        for (Employee emp : employees) {
+            if (emp instanceof ManagerEmployee) {
+                ManagerEmployee m = (ManagerEmployee) emp;
+                System.out.println("Manager effective pay: Rs " + m.effectiveSalary());
+            } else if (emp instanceof InternEmployee) {
+                InternEmployee i = (InternEmployee) emp;
+                System.out.println("Intern effective pay: Rs " + i.effectiveSalary());
+            } else {
+                System.out.println("Plain employee pay: Rs " + emp.getSalary());
+            }
+        }
+    }
+}
